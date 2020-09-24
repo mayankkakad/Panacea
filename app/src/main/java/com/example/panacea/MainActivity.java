d53fca8305b81a0c15c7258724133d63f8a20356
Mayank Kakad <48478111+mayankkakad@users.noreply.github.com>
2020-09-24 22:50:53 +0530

    login, signup UI

--- app/src/main/java/com/example/panacea/MainActivity.java
+++ app/src/main/java/com/example/panacea/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.panacea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup=(Button)findViewById(R.id.button2);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSignUp();
+            }
+        });
+    }
+    public void openSignUp() {
+        Intent intent=new Intent(this,SignUp.class);
+        startActivity(intent);
     }
 }
